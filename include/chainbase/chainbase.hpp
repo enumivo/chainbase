f5683713fc96647d8e2eaea43167c54c6df56db0
enumivo <admin@enumivo.org>
2018-10-18 18:15:49 +0800

    sync for 1.4.1

--- include/chainbase/chainbase.hpp
+++ include/chainbase/chainbase.hpp
@@ -499,6 +499,8 @@ namespace chainbase {
             return {begin, end};
          }
 
+         const auto& stack()const { return _stack; }
+
       private:
          bool enabled()const { return _stack.size(); }
 
@@ -1035,9 +1037,11 @@ namespace chainbase {
 
          bfs::path                                                   _data_dir;
 
+#ifdef CHAINBASE_CHECK_LOCKING
          int32_t                                                     _read_lock_count = 0;
          int32_t                                                     _write_lock_count = 0;
          bool                                                        _enable_require_locking = false;
+#endif
 
          void                                                        _msync_database();
    };
